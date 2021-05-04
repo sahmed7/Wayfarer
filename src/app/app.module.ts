70f59e5449639fda1a6c63b124262fda6551e4b7
Pete Micsunescu <peat.is.nete@gmail.com>
2021-05-04 15:32:19 -0500

    Created 'carousel','cities', & 'navbar' components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CitiesComponent } from './cities/cities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    CitiesComponent
   ],
   imports: [
     BrowserModule,
