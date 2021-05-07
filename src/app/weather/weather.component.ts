b76aa974efaae49d53f531a271d6acaf5bbfabb0
Shaheer Ahmed <sahmed3721@gmail.com>
2021-05-07 13:59:08 -0500

    updated weather api key and some text in weather section

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -24,9 +24,11 @@ export class WeatherComponent implements OnInit {
   }
 
   findWeather(city): any{
-    this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=052f26926ae9784c2d677ca7bc5dec98&&units=imperial`)
+    this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=ffe203e68f45bbb488ed05b65cb0d682&units=imperial`)
         .subscribe(response => this.weather = response);
 }
+
+
 
 
 
